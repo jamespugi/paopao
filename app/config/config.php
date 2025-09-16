077467e70964be44c2f349fa187fb6669f215ffd
Your Name <you@example.com>
2025-09-17 07:48:42 +0800

    m

--- app/config/config.php
+++ app/config/config.php
@@ -79,7 +79,7 @@ $config['ENVIRONMENT']             = 'development';
 | WARNING: You MUST set this value!
 |
 */
-$config['base_url'] 				= 'https://justin-h861.onrender.com';
+$config['base_url'] 				= 'https://paopao-85gf.onrender.com';
 
 /*
 |--------------------------------------------------------------------------
