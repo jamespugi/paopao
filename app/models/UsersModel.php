35ec4a126d9dc8d2908ba7b8276ef59ba7a8fd65
Your Name <you@example.com>
2025-09-17 07:59:45 +0800

    M

--- app/models/UsersModel.php
+++ app/models/UsersModel.php
@@ -7,7 +7,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
  * Automatically generated via CLI.
  */
 class Usersmodel extends Model {
-    protected $table = 'students';
+    protected $table = 'users';
     protected $primary_key = 'id';
 
     public function __construct()
