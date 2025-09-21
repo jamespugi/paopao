020d84a68eb36885a7c894ca5a9f5d8c2b9aab24
Your Name <you@example.com>
2025-09-21 09:38:54 +0800

    m

--- app/config/database.php
+++ app/config/database.php
@@ -61,9 +61,9 @@ $database['main'] = array(
     'driver'	=> 'mysql',
     'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'sql12798318',
-    'password'	=> 'ahyZwYhAnh',
-    'database'	=> 'sql12798318',
+    'username'	=> 'sql12799447',
+    'password'	=> '3U13giQktx',
+    'database'	=> 'sql12799447',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
